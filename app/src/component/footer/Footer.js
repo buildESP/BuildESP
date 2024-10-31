1391304fb220720821da00c9792d673d3e041d7c
BertinBintou <bertin.bintou@epitech.eu>
2024-10-31 14:32:00 +0100

    footer

--- app/src/component/footer/Footer.js
+++ app/src/component/footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer style={{ padding: '20px', textAlign: 'center', background: '#f1f1f1' }}>
-      <p>© 2024 My Website. All Rights Reserved.</p>
+      <p>© 2024 Buildingerie. All Rights Reserved.</p>
       <p>
         <a href="/privacy-policy">Privacy Policy</a> | <a href="/terms-of-service">Terms of Service</a>
       </p>
