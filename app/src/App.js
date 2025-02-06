1baab3778cd4e08730624944f8ed520e8bb6bcb9
Stenalo <stephane-desir.barro@epitech.eu>
2025-02-06 16:13:17 +0100

    fix repo name

--- app/src/App.js
+++ app/src/App.js
@@ -1,35 +1,66 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import FooterC from './components/footerC/FooterC';
+import HeaderC from './components/headerC/HeaderC';
+import AddP from './pages/addP/AddP';
+import DemandP from './pages/demandP/DemandP';
+import EditProfileP from './pages/editProfileP/EditProfileP';
+import HomeP from './pages/homeP/HomeP';
+import LoginP from './pages/loginP/LoginP';
+import MessagesP from './pages/messagesP/MessagesP';
+import ProductDetailsP from './pages/productDetailsP/ProductDetailsP';
+import ProfileP from './pages/profileP/ProfileP';
 
-// Import Component
-import Header from './component/header/Header';
-import Footer from './component/footer/Footer';
-import Home from './component/home/Home';
-import Profile from './component/profile/Profile';
-import Products from './component/products/Poducts';
-import Demand from './component/demand/Demand';
+const App = () => {
+  const [accessToken, setAccessToken] = useState(localStorage.getItem('Token'));
 
+  // Mise à jour de accessToken lors des modifications du localStorage
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setAccessToken(localStorage.getItem('Token'));
+    };
 
+    window.addEventListener('storage', handleStorageChange);
 
-function App() {
+    return () => {
+      window.removeEventListener('storage', handleStorageChange); // Nettoyer l'écouteur
+    };
+  }, []);
+
   return (
-    <Router> {/* Wrap the application in BrowserRouter */}
-      <div className="App">
-        <Header /> {/* This will now have access to the Router context */}
-        <div style={{ padding: '20px' }}>
-          <Routes>
-          <Route path="/" element={<Home />} />
-           <Route path="/profile" element={<Profile />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/demand" element={<Demand />} />  
-          </Routes>
-        </div>
-        <Footer/>
-      </div>
-    </Router>
+    <>
+      {accessToken && <HeaderC />}
+      <Routes>
+        {/* Si le token n'existe pas ou est null, afficher uniquement la route de login */}
+        {!accessToken ? (
+          <>
+            <Route path="*" element={<LoginP />} />
+            <Route path="/login" element={<LoginP />} />
+          </>
+        ) : (
+          <>
+            {/* Routes pour les utilisateurs connectés */}
+            <Route path="/" element={<HomeP />} />
+            <Route path="/home/:category/:subcategory" element={<HomeP />} />
+            <Route path="/home" element={<HomeP />} />
+            <Route path="/profile" element={<ProfileP />} />
+            <Route path="/editprofile" element={<EditProfileP />} />
+            <Route path="/productDetails/:id" element={<ProductDetailsP />} />
+            <Route path="/onDemand" element={<DemandP />} />
+            <Route path="/messages" element={<MessagesP />} />
+            <Route path="/addAnAnnounce" element={<AddP />} />
+            <Route path="/category/*" element={<HomeP />} />
+            
+            {/* Route par défaut pour les chemins incorrects */}
+            <Route path="*" element={<HomeP />} />
+          </>
+        )}
+         
+
+      </Routes>
+      {accessToken && <FooterC />}
+    </>
   );
-}
+};
 
 export default App;
