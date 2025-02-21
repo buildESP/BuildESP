c6afe9f34cf469d7f0579f780c5ff9baf7633d93
Ubuntu <ubuntu@ip-172-31-41-254.eu-west-3.compute.internal>
2025-02-21 12:58:09 +0000

    Ajout de .github au .gitignore pour ne pas le suivre sur EC2

--- backend/api/fixtures/categoryFixtures.js
+++
@@ -1,9 +0,0 @@
-// fixtures/categoryFixtures.js
-
-module.exports = [
-  { id: 1, name: 'Bricolage' },
-  { id: 2, name: 'Maison' },
-  { id: 3, name: 'Jardin' },
-  { id: 4, name: 'Culture' },
-  { id: 5, name: 'High-tech' },
-];
--- cypress/e2e/spec.cy.js
+++
@@ -1,5 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://172.31.41.254:3001/')
-  })
-})
