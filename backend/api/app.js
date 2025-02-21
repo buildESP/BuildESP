7281835ee6ef565f04103cbfbebee1c12c234da1
Antoine Venturini <antoine.venturini@epitech.eu>
2025-02-21 22:18:16 +0100

    soir

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,20 +1,42 @@
-// app.js
-
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const swaggerOptions = require('./swaggerOptions');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const chalk = require('chalk');
 
-const JWT_SECRET = process.env.JWT_SECRET;
 const app = express();
 const port = process.env.PORT || 3000;
 
-// import routes
+// Middleware
+app.use(bodyParser.json());
+
+// Swagger configuration
+const swaggerOptions = require('./swaggerOptions');
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// CORS Configuration
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  'http://35.180.39.100', // IP publique du Front
+  'http://172.31.41.254'  // IP privée du Front
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
+
+// Importation des routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
@@ -23,36 +45,17 @@ const itemRoutes = require('./routes/itemRoutes');
 const exchangeRoutes = require('./routes/exchangeRoutes');
 const groupRoutes = require('./routes/groupRoutes');
 
-
-app.use(bodyParser.json());
-
-// Swagger configuration
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
-// Routes use
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', '172.31.41.254'];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true); // Autorise les origines dans la liste
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-}));
-app.use('/api', userRoutes);
+// Définition des routes
 app.use('/api', authRoutes);
+app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', subcategoryRoutes);
 app.use('/api', itemRoutes);
 app.use('/api', exchangeRoutes);
 app.use('/api', groupRoutes);
 
-app.listen(port, () => {
-  console.log(chalk.green.bold(`🚀 Good job! Buildinguerie API is running on http://localhost:${port}\n`));
-  console.log(chalk.blue(`📚 Docs available at: http://localhost:${port}/doc\n`));
-  console.log(chalk.yellow(`🌐 Environment: ${process.env.NODE_ENV || 'development'}`));
+// Lancement du serveur
+app.listen(port, '0.0.0.0', () => {
+  console.log(chalk.green.bold(`🚀 API is running on http://0.0.0.0:${port}`));
+  console.log(chalk.blue(`📚 Swagger docs: http://0.0.0.0:${port}/doc`));
 });
