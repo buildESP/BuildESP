90abc2c1e17e0f76225746eced88162aa736a02c
Antoine Venturini <antoine.venturini@epitech.eu>
2025-02-27 14:22:12 +0100

    test github action

--- frontend/app/src/components/loginC/LoginC.js
+++ frontend/app/src/components/loginC/LoginC.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axios from 'axios';
 import FooterwavesC from "../footerWavesC/FooterwavesC";
 import "./login.css";
 import { useNavigate } from "react-router-dom";
