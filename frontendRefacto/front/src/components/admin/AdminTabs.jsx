3ef6645eea301cbb821492e37083fa43760fc016
quentin <quentinderycke6@gmail.com>
2025-03-11 22:54:22 +0100

    feat(admin): :construction: init admin Table with data

---
+++ frontendRefacto/front/src/components/admin/AdminTabs.jsx
@@ -0,0 +1,38 @@
+import { Tabs } from "@chakra-ui/react";
+import { LuFolder, LuSquareCheck, LuUser } from "react-icons/lu";
+import AmdinItemTable from "./AdminItemTable";
+import AdminUserTable from "./AdminUserTable";
+import AdminCategoryTable from "./AdminCategoryTable";
+
+const AdminTabs = ({ users, items, categories, refetchUsers, refetchItems, refetchCategories }) => {
+  return (
+    <Tabs.Root defaultValue="items">
+      <Tabs.List>
+        <Tabs.Trigger value="items">
+          <LuUser />
+          Items
+        </Tabs.Trigger>
+        <Tabs.Trigger value="users">
+          <LuFolder />
+          Utilisateurs
+        </Tabs.Trigger>
+        <Tabs.Trigger value="categories">
+          <LuSquareCheck />
+          Catégories et Sous Catégories
+        </Tabs.Trigger>
+      </Tabs.List>
+
+      <Tabs.Content value="items">
+        <AmdinItemTable items={items} refetch={refetchItems} />
+      </Tabs.Content>
+      <Tabs.Content value="users">
+        <AdminUserTable users={users} refetch={refetchUsers} />
+      </Tabs.Content>
+      <Tabs.Content value="categories">
+        <AdminCategoryTable categories={categories} refetch={refetchCategories} />
+      </Tabs.Content>
+    </Tabs.Root>
+  );
+};
+
+export default AdminTabs;
--- frontendRefacto/front/src/pages/AdminPage.jsx
+++ frontendRefacto/front/src/pages/AdminPage.jsx
@@ -1,13 +1,32 @@
-import AdminTabs from "@/components/AdminTabs"
+import useFetchData from "@/hooks/useFetchData";
+import AdminTabs from "@/components/admin/AdminTabs";
+import { Spinner, Text, Box } from "@chakra-ui/react";
 
 const AdminPage = () => {
+  // Chargement des données
+  const { data: users, loading: loadingUsers, error: errorUsers, refetch: refetchUsers } = useFetchData("/users", {requiresAuth: true});
+  const { data: items, loading: loadingItems, error: errorItems, refetch: refetchItems } = useFetchData("/items");
+  const { data: categories, loading: loadingCategories, error: errorCategories, refetch: refetchCategories } = useFetchData("/categories");
 
+  // Gestion du chargement global
+  const isLoading = loadingUsers || loadingItems || loadingCategories;
+  const hasError = errorUsers || errorItems || errorCategories;
 
-    return ( 
-        <>
-        <AdminTabs/>
-        </>
-    )
-}
+  if (isLoading) return <Spinner />;
+  if (hasError) return <Text color="red.500">Erreur de chargement des données.</Text>;
 
-export default AdminPage
+  return (
+    <Box p={6}>
+      <AdminTabs 
+        users={users} 
+        items={items} 
+        categories={categories}
+        refetchUsers={refetchUsers}
+        refetchItems={refetchItems}
+        refetchCategories={refetchCategories}
+      />
+    </Box>
+  );
+};
+
+export default AdminPage;
