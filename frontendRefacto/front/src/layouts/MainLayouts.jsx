a285c636f6e9954771f9208814db775031616e0c
quentin <quentinderycke6@gmail.com>
2025-03-11 22:59:52 +0100

    fix(layout): ensure footer stays at the bottom on all pages

--- frontendRefacto/front/src/layouts/MainLayouts.jsx
+++ frontendRefacto/front/src/layouts/MainLayouts.jsx
@@ -1,20 +1,22 @@
 import SubMenu from "../components/SubMenu";
 import Footer from "../components/navigation/Footer";
 import Navbar from "../components/navigation/Navbar";
+import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router";
 import Breadcrumbs from "../components/navigation/Breadcrumbs";
 const MainLayout = () => {
   return (
-    <>
-      
+          <Box display="flex" flexDirection="column" minH="100vh">
+
     <Navbar />
     <SubMenu/>
     {/* <Breadcrumbs/> */}
-    <Outlet />
-    <Footer/>
+    <Box flex="1">
+        <Outlet />
+      </Box>
+          <Footer/>
   
-
-    </>
+</Box>
   );
 };
 
