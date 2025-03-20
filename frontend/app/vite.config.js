3493931d4355629b8aff178abc3ac981dfcf4f12
Antoine Venturini <antoine.venturini@epitech.eu>
2025-03-20 16:05:22 +0100

    nom de domain

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -17,21 +17,13 @@ export default defineConfig(({ mode }) => {
       __APP_ENV__: env.APP_ENV,
     },
     server: {
-      host: true, // Permet au serveur de Vite d'être accessible depuis des hôtes externes
-      port: 5173, // Utilisation du port 5173 pour le serveur frontend
+      host: '0.0.0.0',  // Permet d'accepter les connexions externes
+      port: 5173,  // Port d'écoute du frontend
       allowedHosts: [
         'neighborrow.hephel.fr',  // Hôte sans www
         'www.neighborrow.hephel.fr',  // Hôte avec www
         'localhost',  // Accès local
       ],
-      proxy: {
-        // Si tu veux utiliser Vite en mode dev avec un proxy vers Nginx
-        '/api': {
-          target: 'http://localhost', // Remplace avec l'adresse de ton serveur backend ou Nginx
-          changeOrigin: true,
-          secure: false,
-        },
-      },
     },
   };
 });
