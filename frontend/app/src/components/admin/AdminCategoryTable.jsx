bb23c6805acd01ab062fdcc8da064d3dc340fc61
Stenalo <stephane-desir.barro@epitech.eu>
2025-03-20 10:01:47 +0100

    trying to fix conflicts

--- frontend/app/src/components/admin/AdminCategoryTable.jsx
+++
@@ -1,7 +0,0 @@
-import AdminTable from "./AdminTable";
-
-const AdminCategoryTable = ({ categories, refetch }) => {
-  return <AdminTable items={categories} refetch={refetch} />;
-};
-
-export default AdminCategoryTable;
--- frontend/app/src/components/admin/AdminItemTable.jsx
+++
@@ -1,7 +0,0 @@
-import AdminTable from "./AdminTable";
-
-const AdminItemTable = ({ items, refetch }) => {
-  return <AdminTable items={items} refetch={refetch} />;
-};
-
-export default AdminItemTable;
