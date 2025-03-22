9d79125679ba217bab6554298950dde523f78c3e
Antoine Venturini <antoine.venturini@epitech.eu>
2025-03-22 11:24:06 +0100

    test2

--- frontend/app/vite.config.js
+++ frontend/app/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
@@ -19,7 +18,18 @@ export default defineConfig(({ mode }) => {
       __APP_ENV__: env.APP_ENV,
     },
     server: {
-      allowedHosts: true
+      host: '0.0.0.0', // Écouter sur toutes les interfaces réseau
+      port: 5173,
+      hmr: {
+        host: 'neighborrow.hephel.fr', // Remplacez par votre domaine ou adresse IP publique
+        protocol: 'ws',
+        clientPort: 5173 // Assurez-vous que ce port est ouvert et accessible
+      },
+      allowedHosts: [
+        'neighborrow.hephel.fr',
+        'www.neighborrow.hephel.fr',
+        '13.39.105.132'
+      ]
     }
   };
 });
