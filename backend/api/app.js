eed7f0ec85e737e25df05e35b9b02900b5ca2afb
Antoine Venturini <antoine.venturini@epitech.eu>
2025-04-06 17:08:47 +0200

    test connexion S3 2

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,20 +1,16 @@
-// app.js
-
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerOptions = require('./swaggerOptions');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const chalk = require('chalk');
 
-const JWT_SECRET = process.env.JWT_SECRET;
 const app = express();
 const port = process.env.PORT || 3000;
 
-// import routes
+// Import routes
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
@@ -24,15 +20,34 @@ const exchangeRoutes = require('./routes/exchangeRoutes');
 const imageRoutes = require('./routes/imageRoutes');
 const groupRoutes = require('./routes/groupRoutes');
 
-
 app.use(bodyParser.json());
 
 // Swagger configuration
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
+// CORS configuration
+const allowedOrigins = [
+  'http://neighborrow.hephel.fr',
+  'https://neighborrow.hephel.fr',
+  'http://localhost:3000',
+  'http://localhost:5173',
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization',
+}));
+
 // Routes use
-app.use(cors({ origin: true }));
 app.use('/api', userRoutes);
 app.use('/api', authRoutes);
 app.use('/api', categoryRoutes);
@@ -40,12 +55,10 @@ app.use('/api', subcategoryRoutes);
 app.use('/api', itemRoutes);
 app.use('/api', exchangeRoutes);
 app.use('/api', groupRoutes);
-app.use('/api/', imageRoutes);
-
+app.use('/api', imageRoutes);
 
 app.listen(port, () => {
   console.log(chalk.green.bold(`🚀 Good job! Buildinguerie API is running on http://localhost:${port}\n`));
   console.log(chalk.blue(`📚 Docs available at: http://localhost:${port}/doc\n`));
   console.log(chalk.yellow(`🌐 Environment: ${process.env.NODE_ENV || 'development'}`));
 });
- 
