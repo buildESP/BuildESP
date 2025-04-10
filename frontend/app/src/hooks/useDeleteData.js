bccb694f6232d31c15f7b49d132cfabbce7cd461
Antoine Venturini <antoine.venturini@epitech.eu>
2025-04-10 14:53:35 +0200

    test add deletdata

--- frontend/app/src/hooks/useDeleteData.js
+++ frontend/app/src/hooks/useDeleteData.js
@@ -5,7 +5,12 @@ import { toast } from "react-toastify";
 // Utilisez la variable d'environnement pour la base URL de l'API et ajoutez le port et le chemin
 const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}:/api`;
 
-const useDeleteData = (endpoint) => {
+/**
+ * Hook personnalisé pour supprimer des données via l'API.
+ * @param {string} basePath - Le chemin de base de l'API (ex: "/items").
+ * @returns {Object} - { deleteData, loading, error }
+ */
+ const useDeleteData = (basePath) => {
   const { token } = useAuth();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
