e8e51318d46b16daab2c62596dd594bc098ca8b8
quentin <quentinderycke6@gmail.com>
2025-04-17 23:27:06 +0200

    fix: :bug: delete double import in appRoute

--- frontend/app/src/routes/AppRoutes.jsx
+++ frontend/app/src/routes/AppRoutes.jsx
@@ -22,7 +22,6 @@ import ConditionOfUs from "@/pages/ConditionOfUs";
 import ExchangesPage from "../pages/ExchangesPage";
 import ChatPage from "../pages/ChatPage";
 import MentionLegales from "@/pages/MentionLegales";
-import ExchangesPage from "@/pages/ExchangesPage";
 
 /**
  * @component AppRoutes
